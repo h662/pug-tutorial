94af93c6c3792d908a79119354018a5fd4dfdaf9
sh183694@gmail.com <sh183694@gamil.com>
2019-11-21 12:49:15 +0900

    ADD req

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ app.set("view engine", "pug");
 app.set("views", "./views");
 
 app.get("/", (req, res) => {
-  res.render("index", { title: "Hey", message: "Hello there!" });
+  res.render("index", { title: "Hey", message: "Hello there!", time: Date() });
+});
+
+app.get("/topic", (req, res) => {
+  res.send(req.query.id + "," + req.query.name);
 });
 
 app.listen(3000, () => {
