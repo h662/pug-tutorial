3d232f5c2d4fa495de6e71001310722e0a8824bb
sh183694@gmail.com <sh183694@gamil.com>
2019-11-26 15:56:07 +0900

    ADD params

--- app.js
+++ app.js
@@ -8,15 +8,19 @@ app.get("/", (req, res) => {
   res.render("index", { title: "Hey", message: "Hello there!", time: Date() });
 });
 
-app.get("/topic", (req, res) => {
+app.get("/topic/:id", (req, res) => {
   var topics = ["Javascript is ...", "Nodejs is ...", "Express is ..."];
   var output = `
     <a href="/topic?id=0">Javascript</a><br>
     <a href="/topic?id=1">Nodejs</a><br>
     <a href="/topic?id=2">Express</a><br>
-    ${topics[req.query.id]}
+    ${topics[req.params.id]}
   `;
   res.send(output);
+});
+
+app.get("/topic/:id/:mode", (req, res) => {
+  res.send(req.params.id + "," + req.params.mode);
 });
 
 app.listen(3000, () => {
